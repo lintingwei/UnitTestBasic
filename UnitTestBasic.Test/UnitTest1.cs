fffbd18802e079e669f5dab1fb2a95b3fa9d8466
JerryLin <bggtank0102@gmail.com>
2022-01-15 16:14:43 +0800

    init project

---
+++ UnitTestBasic.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace UnitTestBasic.Test
+{
+    public class SimpleParserTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
