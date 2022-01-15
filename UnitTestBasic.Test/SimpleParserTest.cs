7fb0dd3ec5a2beb318055056206c9f87e6d4ff3d
JerryLin <bggtank0102@gmail.com>
2022-01-15 17:42:21 +0800

    refactor and add comment

--- UnitTestBasic.Test/SimpleParserTest.cs
+++ UnitTestBasic.Test/SimpleParserTest.cs
@@ -4,27 +4,36 @@ namespace UnitTestBasic.Test
 {
     public class SimpleParserTest
     {
-        [Test]
-        public void SimpleParser_Should_Return_0()
+        private SimpleParser _parser;
+
+        [SetUp]
+        public void SetUp()
         {
             // Arrange
-            var parser = new SimpleParser();
+            _parser = new SimpleParser();
+        }
 
+        [Test]
+        // Verify return value
+        public void SimpleParser_Should_Return_0()
+        {
             // Act
-            var result = parser.ParseAndSum("0");
+            var result = CallParseAndSum();
 
             // Assert
             Assert.AreEqual(0, result);
         }
-
         [Test]
+        // Verify object status
         public void SimpleParser_IsParserAndSumCalled_Should_Be_True()
         {
-            var parser = new SimpleParser();
+            CallParseAndSum();
 
-            parser.ParseAndSum(string.Empty);
-
-            Assert.IsTrue(parser.IsParseAndSumCalled);
+            Assert.IsTrue(_parser.IsParseAndSumCalled);
+        }
+        private int CallParseAndSum()
+        {
+            return _parser.ParseAndSum("0");
         }
     }
 }
