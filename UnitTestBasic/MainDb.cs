f0ddb597b969f0a8ac3b3479d5845a3f0f6856e5
JerryLin <bggtank0102@gmail.com>
2022-01-16 17:29:21 +0800

    SKA VerifyPassword

---
+++ UnitTestBasic/MainDb.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace UnitTestBasic
+{
+    public class MainDb
+    {
+        public static DataTable GetPasswordBy(string username)
+        {
+            using var conn = new SqlConnection("Data Source=.;Initial Catalog=LogDb;Persist Security Info=True;User ID=N_Logger;Password=log123;Pooling=true;min pool size=4;max pool size=32;");
+            conn.Open();
+
+            using var cmd = new SqlCommand("dbo.GetPasswordByUsername", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters["@username"].Value = username;
+            using SqlDataReader dr = cmd.ExecuteReader();
+            var dt = new DataTable();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
---
+++ UnitTestBasic/SkaLegacyCode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestBasic
+{
+    public class SkaLegacyCode
+    {
+        public bool VerifyPassword(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+
+            var dt = MainDb.GetPasswordBy(username);
+
+            if (dt == null || dt.Rows.Count != 1)
+            {
+                return false;
+            }
+
+            var dbPassword = dt.Rows[0]["password"].ToString();
+
+            return !string.IsNullOrEmpty(dbPassword) && dbPassword.Equals(password);
+        }
+    }
+}
